0ff5c51ecc93a6c36ee5482829ed7b68e3dba061
JGOON1 <john.meng014@gmail.com>
2020-04-01 15:47:51 -0400

    app is connected to database, able to add items, working on deleting items on the app

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // connecting to mongodb
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB", { 
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
 
 const itemsSchema = {
   name: String
@@ -20,27 +23,19 @@ const itemsSchema = {
 const Item = mongoose.model("Item", itemsSchema);
 
 // Mongoose Document
-const item1 = new Item ({
+const item1 = new Item({
   name: "Welcome to your todo list."
 });
 
-const item2 = new Item ({
+const item2 = new Item({
   name: "Practice coding"
 });
 
-const item3 = new Item ({
+const item3 = new Item({
   name: "take a break"
 });
 
 const defaultItems = [item1, item2, item3];
-Item.insertMany(defaultItems, function(err){
-  if (err){
-    console.log(err);
-  } else {
-    console.log("Sucessfully saved default items to database")
-  }
-});
-
 
 
 
@@ -48,34 +43,53 @@ Item.insertMany(defaultItems, function(err){
 const items = ["Buy Food", "Cook Food", "Eat Food"];
 const workItems = [];
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
 
+  //contain things inside Item collection
+  Item.find({}, function (err, foundItems) {
 
-  
-const day = date.getDate();
+    if (foundItems.length === 0) {
+      //inserting item into the Item model
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Sucessfully saved default items to database")
+        }
+      });
+      res.redirect("/");
+    } else {
 
-  res.render("list", {listTitle: "Today", newListItems: items});
-
-});
-
-app.post("/", function(req, res){
-
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
   }
+
+  })
+
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.post("/", function (req, res) {
+
+
+  // grabbing the new name from input
+  const itemName = req.body.newItem;
+
+
+// creating a new document
+  const item = new Item({
+    name: itemName,
+  });
+
+  item.save();
+  
+  res.redirect("/");
+
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
